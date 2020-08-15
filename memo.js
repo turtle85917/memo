3d8c892fa3a3f46a983b9f13f43828bb811c0e66
turtle85917 <61219276+turtle85917@users.noreply.github.com>
2020-08-15 10:19:45 +0900

    Update memo.js

--- memo.js
+++ memo.js
@@ -1,1 +1,8 @@
+const quick = require('quick.db')
+const { Client, MessageEmbed } = require('discord.js')
 
+const client = new Client()
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is login!`)
+})
