caba23a582578410faf2a3202316829b0e3133a6
slime_turtle <61219276+turtle85917@users.noreply.github.com>
2020-12-06 11:33:47 +0900

    quick.db 메모
    quick.db 메모 입니다!

--- quick.db/memo.js
+++ quick.db/memo.js
@@ -10,9 +10,9 @@ client.on('ready', async () => {
 client.on('message', async (message) => {
   if (message.author.bot || message.system) return;
   let prefix = process.env.prefix;
+  let memo = message.content.split(' ').slice(2).join(' ');
   
   if (message.content.startsWith(`${prefix} 메모`)) {
-    let memo = message.content.split(' ').slice(2).join(' ')
     if(db.get(`${memo}_${message.author.id}`)) return message.channel.send(`\`${memo}\`(은)는 이미 노트에 저장되어 있어요!`)
     db.set(`${memo}_${message.author.id}`, memo)
     message.channel.send(`\`${memo}\`(을)를 노트에 적어놨어요!`)
